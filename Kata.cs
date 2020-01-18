73af1891cb50a827f51593268f5c275c5d12cdb5
Santosh Balaaji <sv.santosh2695@gmail.com>
2020-01-19 12:58:31 +1300

    Kata Completed

---
+++ Kata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OccurencesOfArray
+{
+    public class Kata
+    {
+        public static int[] FindAll(int[] array, int n)
+        {
+
+            List<int> arrayList = new List<int>();
+
+
+            for (int j = 0; j < array.Length; j++)
+            {
+               
+                    if (array[j] == n)
+                    {
+                        arrayList.Add(j);
+                    }
+                
+            }
+
+            return arrayList.ToArray();
+        }
+    }
+}
